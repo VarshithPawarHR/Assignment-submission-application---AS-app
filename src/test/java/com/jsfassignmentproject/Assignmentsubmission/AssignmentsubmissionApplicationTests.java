83daf0015db3d8f8edc8138de7f1e22b27fc5d1c
varshithpawarhr <varshithpawarhr@gmail.com>
2024-08-15 00:12:32 +0530

    initial springboot setup

---
+++ src/test/java/com/jsfassignmentproject/Assignmentsubmission/AssignmentsubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsfassignmentproject.Assignmentsubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentsubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
